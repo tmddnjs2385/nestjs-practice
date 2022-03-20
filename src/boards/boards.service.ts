fca2736319bd2690d4f85bebe573e1fb3fe96578
OptimusLee <36293518+tmddnjs2385@users.noreply.github.com>
2022-03-20 12:06:44 +0900

    deleteBoard

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -27,4 +27,12 @@ export class BoardsService {
 
     return board;
   }
+
+  getBoardById(id: string): Board {
+    return this.boards.find((board) => board.id === id);
+  }
+
+  deleteBoard(id: string): void {
+    this.boards = this.boards.filter((board) => board.id !== id);
+  }
 }
